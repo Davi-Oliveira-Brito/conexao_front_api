0d8fa480d5389ebee1d2278635b0b75a4acfcd9e
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-05-25 17:51:28 -0300

    teste2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import SiteRoutes from './siteRoutes';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< HEAD
     <SiteRoutes/>
+=======
+    <div>Rodando de merda nesse carai</div>
+>>>>>>> e92f6244fd956bf7d5132e1a9bd5707c7dd8e768
   </React.StrictMode>
 );
 
